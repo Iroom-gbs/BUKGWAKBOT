af187a70b818638625df50bac583a887955957b2
hegelty <hegelty@naver.com>
2021-03-28 01:15:46 +0900

    급식 오류 해결, 시간표/한강수온 기능 추가, 라이선스 추가

--- KakaoBot.js
+++ KakaoBot.js
@@ -1,9 +1,41 @@
+/**
+ * 이뤘다
+ * 제작자 : HegelTY
+ * 1학년 2반을 위해 만들어진 카톡봇입니다.
+ * 현재 버전 dev3(20210327)
+ * 
+ * 어짜피 이 코드 볼사람 나말고 두명밖에 없을거 같긴 함
+ * MIT라이선스니까 너희도 내 코드 가져다쓸거면 출처 표기하셈
+ * 
+ * © 2021 Hegel, All rights reserved.
+ * MIT License
+Copyright (c) 2021 Hegel. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights 
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 importClass(org.jsoup.Jsoup);
 const scriptName = "test";
+const comci = require('comci.js');
+
 var MealDataDate;
 var breakfast_Result = String("");
 var lunch_Result = String("");
 var dinner_Result = String("");
+
+var hangang_temp, HangangDataDate;
 
 /**
  * (string) room
@@ -15,7 +47,8 @@ var dinner_Result = String("");
  * (string) packageName
  */
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-  msg_data = msg.split(' ')
+  msg_data = msg.split(' ');
+
   if(msg_data[0]=='!반장')
   {
     replier.reply("명물");
@@ -30,11 +63,59 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
   }
   else if(msg_data[0]=="!도움말")
   {
-    replier.reply("!과제 : 과제를 보여줍니다.\n!급식 [아침/점심/저녁] : 급식을 보여줍니다.")
+    replier.reply("도움말\n"
+                + "!과제 : 과제를 보여줍니다.\n"
+                + "!급식 (아침/점심/저녁) : 급식을 보여줍니다.\n"
+                + "!시간표 (내일) : 시간표를 보여줍니다.\n"
+                + "!한강수온 : 오늘 한강의 온도를 알려줍니다."
+                );
   }
   else if(msg_data[0]=='!과제')
   {
     replier.reply("\u200b".repeat(500) + "아직 없음");
+  }
+  else if(msg_data[0]=='!한강수온')
+  {
+    let today = new Date();
+    let year = String(today.getFullYear()); // 년도
+    let month = numberPad(today.getMonth() + 1, 2);  // 월
+    let date = numberPad(today.getDate(), 2);  // 날짜
+    
+    if(HangangDataDate != year+month+date) //한강 수온 데이터가 최신이 아닐 경우 갱신
+    {
+      let HangangData = Jsoup.connect("https://api.hangang.msub.kr/").ignoreContentType(true).get().text();           
+      hangang_temp = HangangData.split('"')[11];
+      HangangDataDate = year+month+date;
+    }
+    replier.reply("오늘 한강의 수온은 " + hangang_temp +"℃"+ "(" + String(Number(hangang_temp)+273) +"K) 입니다.");
+  }
+  else if(msg_data[0]=="!시간표")
+  {
+    let today = new Date();
+    let year = String(today.getFullYear()); // 년도
+    let month = numberPad(today.getMonth() + 1, 2);  // 월
+    let date = numberPad(today.getDate(), 2);  // 날짜
+    var day = today.getDay() - 1; //요일
+    
+    if(msg_data[1]=="내일") day+=1;
+    if(day==6) day=-1;
+    
+    var TimeTable = String("");
+    
+    var TimeTableData = comci.getTimeTable(12045,1,2); //시간표 받아오기
+    
+    if(day==-1||day==5) replier.reply("주말 시간표\n학원가셈");
+    else
+    {
+      var i=0;
+      var TimeTable = String("");
+      for(i=0;i<7;i++)
+      {
+        //replier.reply(TimeTableData.시간표[day][i]);
+        TimeTable+= String(i+1)+"교시 : " + TimeTableData.시간표[day][i] + "\n";
+      }
+      replier.reply(year+"년 "+month+"월 "+date+"일 시간표\n"+TimeTable);
+    }
   }
   else if(msg_data[0]=="!급식")
   {
@@ -42,8 +123,8 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     let year = String(today.getFullYear()); // 년도
     let month = numberPad(today.getMonth() + 1, 2);  // 월
     let date = numberPad(today.getDate(), 2);  // 날짜
-    let day = today.getDay; //요일
-
+    var day = today.getDay(); //요일
+    
     if(day==0||day==6)
     {
       random_Num = Math.random() * 10;
@@ -58,27 +139,37 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
       else if(random_Num <= 9) replier.reply("안알려줌");
       else if(random_Num <= 10) replier.reply("우리가 어떤민족입니까!");
     }
-    
-    if(MealDataDate != year+month+date)
-    {
-      replier.reply("급식 정보를 받아오는 중입니다.\n시간이 걸릴 수 있습니다.");
-      RenewMealData(year, month, date);
-    }
-    if(msg_data[1] == "아침")
-    {
-      replier.reply("아침\n--------\n" + "\u200b".repeat(500) + breakfast_Result);
-    }
-    else if(msg_data[1] == "점심")
-    {
-      replier.reply("점심\n--------\n" + "\u200b".repeat(500) + lunch_Result);
-    }
-    else if(msg_data[1] == "저녁")
-    {
-      replier.reply("저녁\n--------\n" + "\u200b".repeat(500) + dinner_Result);
-    }
     else
     {
-      replier.reply("오늘의 메뉴\n\n" + "\u200b".repeat(500) + "아침\n--------\n" +  breakfast_Result + "\n\n점심\n--------\n" +  lunch_Result + "\n\n저녁\n--------\n" +  dinner_Result);
+      if(MealDataDate != year+month+date) //급식 정보가 최신이 아닐 경우 갱신
+      {
+        replier.reply("급식 정보를 받아오는 중입니다.\n시간이 걸릴 수 있습니다.");
+        RenewMealData(year, month, date);
+      }
+      if(msg_data[1] == "아침")
+      {
+        replier.reply("아침\n--------\n" + breakfast_Result);
+      }
+      else if(msg_data[1] == "점심")
+      {
+        replier.reply("점심\n--------\n" + lunch_Result);
+      }
+      else if(msg_data[1] == "저녁")
+      {
+        replier.reply("저녁\n--------\n" + dinner_Result);
+      }
+      else
+      {
+        replier.reply("오늘의 메뉴\n\n"
+                    + "\u200b".repeat(500)
+                    + "아침\n--------\n"
+                    + breakfast_Result
+                    + "\n\n점심\n--------\n"
+                    + lunch_Result
+                    + "\n\n저녁\n--------\n"
+                    + dinner_Result
+                    );
+      }
     }
   }
   /*
@@ -138,8 +229,9 @@ function RenewMealData(year, month, date)
                                   ,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
                                   ,false
                                   ,false
-                                  )
+                                  );
 
+  //아침                
   var breakfast_Data = ((((meal_Data.split("<DDISH_NM>"))[1]
                           .split("</DDISH_NM>"))[0])
                           .replace("<![CDATA[","")
@@ -149,6 +241,8 @@ function RenewMealData(year, month, date)
   {
     breakfast_Result+=(breakfast_Data[i].split("*"))[0] + "\n";
   }
+
+  //점심
   var lunch_Data = ((((meal_Data.split("<DDISH_NM>"))[2]
                                 .split("</DDISH_NM>"))[0])
                                 .replace("<![CDATA[","")
@@ -159,7 +253,10 @@ function RenewMealData(year, month, date)
     lunch_Result+=(lunch_Data[i].split("*"))[0] + "\n";
   }
 
+  //저녁
+  //저녁이 없는 금요일
   if(meal_Data.split("<DDISH_NM>").length<=3) dinner_Result+="오늘은 저녁밥이 없습니다.";
+  //저녁이 있는 금요일
   else
   {
     var dinner_Data = ((((meal_Data.split("<DDISH_NM>"))[3]
@@ -173,6 +270,7 @@ function RenewMealData(year, month, date)
     }
   }
 
+  //갱신일시 저장
   MealDataDate = year+month+date;
 }
 
