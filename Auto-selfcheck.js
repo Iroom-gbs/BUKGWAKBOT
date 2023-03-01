bde9dc529c2a6a94c1d1968ee8ac910a45cf83c9
hegelty <hegelty@naver.com>
2023-03-01 16:55:04 +0900

    부활

--- Auto-selfcheck.js
+++ Auto-selfcheck.js
@@ -1,31 +1,31 @@
 importClass(org.jsoup.Jsoup);
-const scriptName = "autoselfcheck";
+const scriptName = "autoselfcheck";
 
-let selfcheck_route = "/sdcard/BUKGWAKBOT/selfcheck.json";
-const FS = FileStream;
+let selfcheck_route = "/sdcard/BUKGWAKBOT/selfcheck.json";
+const FS = FileStream;
 
-var result = "";
+var result = "";
 
-function Autoselfcheck_Function() {
-  result="";
-  selfcheckList = JSON.parse(FS.read(selfcheck_route));
-  for(i=0;i<selfcheckList.인원;i++) {
-      let name = (selfcheckList.데이터)[i].이름;
-      let birth = (selfcheckList.데이터)[i].생일;
-      let pass = (selfcheckList.데이터)[i].비밀번호;
-      let number = (selfcheckList.데이터)[i].번호;
-      status = Jsoup.connect("http://121.168.91.34:8000/hcs?name="
-                              + name
-                              + "&birth=" + birth
+function Autoselfcheck_Function() {
+  result="";
+  selfcheckList = JSON.parse(FS.read(selfcheck_route));
+  for(i=0;i<selfcheckList.인원;i++) {
+      let name = (selfcheckList.데이터)[i].이름;
+      let birth = (selfcheckList.데이터)[i].생일;
+      let pass = (selfcheckList.데이터)[i].비밀번호;
+      let number = (selfcheckList.데이터)[i].번호;
+      status = Jsoup.connect("http://121.168.91.34:8000/hcs?name="
+                              + name
+                              + "&birth=" + birth
                               + "&local=%EA%B2%BD%EA%B8%B0&school=%EA%B2%BD%EA%B8%B0%EB%B6%81%EA%B3%BC%ED%95%99%EA%B3%A0&type=%EA%B3%A0%EB%93%B1&"
-                              + "&password=" + pass
-                              ).ignoreContentType(true).get().text();
-      if(JSON.parse(status).code == "SUCCESS") {
-          result += number + name + " 성공\n";
-      }
-      else {
-          result += number + name + " " + JSON.parse(status).code + "\n";
-      }
-  }
-  return result;
+                              + "&password=" + pass
+                              ).ignoreContentType(true).get().text();
+      if(JSON.parse(status).code == "SUCCESS") {
+          result += number + name + " 성공\n";
+      }
+      else {
+          result += number + name + " " + JSON.parse(status).code + "\n";
+      }
+  }
+  return result;
 }
---
+++ manager.js
@@ -0,0 +1,33 @@
+var roomList = [];
+var scriptList = ["Kaling","DiTalks","gamemodule", "module", "main"]
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+  if(isGroupChat&&roomList.indexOf(room)==-1) {
+    Log.d(room)
+    roomList.push(room);
+  }
+  if(sender=="진짜나태양임"&&room=="진짜관리방") {
+    if(msg.startsWith("!공지")) {
+      for(let i=0;i<roomList.length;i++) {
+        try {
+          replier.reply(roomList[i], "📢 공지사항\n============\n" + msg.substr(3));
+        } catch(e) {
+          replier.reply(roomList[i]+" 에 메세지를 보낼 수 없습니다.");
+        }
+        java.lang.Thread.sleep(5000);
+      }
+    }
+    if(msg=="!방목록") {
+      replier.reply(roomList.length + "개 방\n---------\n" + roomList.join("\n"));
+    }
+    if(msg=="!리로드") {
+      for(let i=0;i<scriptList.length;i++) {
+        Api.reload(scriptList[i]);
+        if(i==0) java.lang.Thread.sleep(1000*3);
+        Api.on(scriptList[i]);
+        replier.reply(scriptList[i] + "리로드 완료");
+        java.lang.Thread.sleep(300);
+        }
+    }
+  }
+  
+}
